0033ade70337dce04a56f3a280dd659da277e628
Fabrice Leal <fabrice.leal.ch@gmail.com>
2012-12-26 14:42:57 -0800

    simple examples

--- 04/functions.hs
+++ 04/functions.hs
@@ -1,3 +1,15 @@
-lucky :: Int -> String
+-- Use something like (...) => ...
+-- to use constraints
+
+lucky :: (Integral a) => a -> String
 lucky 7 = "Lucky number 7!"
 lucky _ = "Call me with 7!"
+
+sayme :: (Integral a) => a -> String
+sayme 1 = "one"
+sayme 2 = "two"
+sayme 3 = "three"
+sayme 4 = "four"
+sayme 5 = "five"
+sayme _ = "not between one and four..."
+

352700d522152e871176780c389af6b6c82b517a
Fabrice Leal <fabrice.leal.ch@gmail.com>
2012-12-26 14:22:31 -0800

    started types and typeclasses

---
+++ 03/types_and_typeclasses.hs
@@ -0,0 +1,14 @@
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A' .. 'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+-- Int  - fixed size
+-- Integer - arbitrary size
+-- Float
+-- Double
+-- Bool
+-- Char
+

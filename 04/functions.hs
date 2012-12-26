862d86dcf8db2a6551df2d8d23874b4cb2ca5e33
Fabrice Leal <fabrice.leal.ch@gmail.com>
2012-12-26 14:48:58 -0800

    oneOfxxx

--- 04/functions.hs
+++ 04/functions.hs
@@ -19,3 +19,12 @@ factorial x = x * factorial (x - 1)
 
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (a1, a2) (b1, b2) = (a1 + b1, a2 + b2)
+
+oneOfTriple :: (a, b, c) -> a
+oneOfTriple (f, _, _) = f
+
+twoOfTriple :: (a, b, c) -> b
+twoOfTriple (_, s, _) = s
+
+threeOfTriple :: (a, b, c) -> c
+threeOfTriple (_, _, t) = t

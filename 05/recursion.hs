857ef6f6f0f9b1ccd42f0976ca5cf007a6e4fa34
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-12 15:09:41 -0800

    started playing with recursion

---
+++ 05/recursion.hs
@@ -0,0 +1,23 @@
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "empty list!"
+maximum' [x] = x
+maximum' (x:xs)
+	 | x > maxTail = x
+	 | otherwise = maxTail
+	 where maxTail = maximum' xs
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+	   | n <= 0 = []
+	   | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+      | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]

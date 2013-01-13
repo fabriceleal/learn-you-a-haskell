ef8ccbe64201187892225ec012a14f9554cb732f
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-13 11:49:22 -0800

    about to start typenames 102

--- 08/typenames.hs
+++ 08/typenames.hs
@@ -87,3 +87,43 @@ lockers = Map.fromList
 	,(109, (Free, "893JJ"))
 	,(110, (Free, "99292"))
 	]
+
+
+--data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+a = 4 :-: 5 :-: 6 :-: Empty
+b = 7 :-: 8 :-: 9 :-: Empty
+c = a .++ b
+
+-- ***
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+	   | x == a = Node x left right
+	   | x < a  = Node a (treeInsert x left) right
+	   | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+	 | x == a = True
+	 | x < a  = treeElem x left
+	 | x > a  = treeElem x right
+
+nums = [8, 6, 4, 1, 7, 3, 5]
+numsTree = foldr treeInsert EmptyTree nums
+have3 = 3 `treeElem` numsTree
+have10 = 10 `treeElem` numsTree

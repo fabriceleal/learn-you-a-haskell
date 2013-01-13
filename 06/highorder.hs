05d3926f839ac0d9a0e0011afcc75020ab16e2e3
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-12 16:22:44 -0800

    before entering lambdas

--- 06/highorder.hs
+++ 06/highorder.hs
@@ -68,3 +68,16 @@ quicksort (x:xs) =
 	      bigger = quicksort (filter (> x) xs)
 	  in smaller ++ [x] ++ bigger
 
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999 ..])
+		 where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+      | even n = n:chain (n `div` 2)
+      | odd n = n:chain (n * 3 + 1)
+
+listOfFuns = map (*) [0..]
+res18 = (listOfFuns !! 4) 5
+

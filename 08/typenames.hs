c62cf443be477a8ecfa340012b2af85e89069ebb
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-13 11:27:12 -0800

    about to start recursive data structures

--- 08/typenames.hs
+++ 08/typenames.hs
@@ -60,7 +60,7 @@ inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
 
 -- ***
 
-data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
+--data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
 
 -- ***
 
@@ -69,3 +69,21 @@ data LockerState = Taken | Free deriving (Show, Eq)
 type Code = String
 
 type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+	     case Map.lookup lockerNumber map of
+	     	  Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesnt exist!"
+		  Just (state, code) -> if state /= Taken
+		       	       	     	then Right code
+					else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+	[(100, (Taken, "ZD39I"))
+	,(101, (Free, "JAH3I"))
+	,(103, (Free, "IQSA9"))
+	,(105, (Free, "QOTSA"))
+	,(109, (Free, "893JJ"))
+	,(110, (Free, "99292"))
+	]

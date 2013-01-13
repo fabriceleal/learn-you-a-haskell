4de16e20e9e8dd4ba9f527db82a70070d1e99e55
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-12 16:13:23 -0800

    quicksort with filter

--- 06/highorder.hs
+++ 06/highorder.hs
@@ -40,3 +40,31 @@ flip' f y x = f x y
 
 r8 = flip' zip [1, 2, 3, 4, 5] "hello"
 r9 = zipWith (flip' div) [2, 2..] [10, 8, 6, 4, 2]
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+r10 = map (+3) [1, 2, 3, 4]
+r11 = map (++ "!") ["BANG", "POW", "BIFF"]
+r12 = map (replicate 3) [3..6]
+r13 = map (map (^2)) [[1,2], [3,4,5,6], [7,8]]
+r14 = map fst [(1,2), (3,4), (5,6), (7,8), (9,0)]
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+	| p x = x : filter' p xs
+	| otherwise = filter' p xs
+
+r15 = filter (>3) [1,5,3,2,9,7,8]
+r16 = filter (==3) [1,5,3,2,9,7,8]
+r17 = filter even [1..10]
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+	  let smaller = quicksort (filter (<= x) xs)
+	      bigger = quicksort (filter (> x) xs)
+	  in smaller ++ [x] ++ bigger
+

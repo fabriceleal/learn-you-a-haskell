2fcfba12c9bca877bb699f8c1356b508cda2a2e3
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-12 16:02:39 -0800

    finished fst batch of example

--- 06/highorder.hs
+++ 06/highorder.hs
@@ -16,3 +16,27 @@ add1 = (+1)
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
 
+r1 = applyTwice (++ "B") "A"
+r2 = applyTwice ("A" ++) "B"
+r3 = applyTwice (3:) [1]
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+r4 = zipWith' (+) [1,2,3] [4,5,6]
+
+f :: a -> b -> (a, b)
+f a b = (a, b)
+
+r5 = zipWith' (+) [1,2,3] [4,5,6]
+r6 = zipWith' f [1,2,3] [4,5,6]
+r7 = zipWith' (++) ["joy ", "sonic ", "massic "] ["division", "youth", "attack"]
+
+-- Which is the same thing as flip' :: (a -> b -> c) -> b -> a -> c
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+r8 = flip' zip [1, 2, 3, 4, 5] "hello"
+r9 = zipWith (flip' div) [2, 2..] [10, 8, 6, 4, 2]

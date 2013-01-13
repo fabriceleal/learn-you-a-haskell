5e65511258afe95c68c26cba5f1a1920c29c12ba
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-13 11:06:32 -0800

    going into typenames ...

--- 08/typenames.hs
+++ 08/typenames.hs
@@ -1,7 +1,42 @@
-data Point = Point Float Float deriving(Show)
-data Shape = Circle Point Float | Rectangle Point Point deriving(Show)
+-- record syntax
+data Person = Person {
+     	    firstName :: String,
+	    lastName :: String,
+	    age :: Int
+     } deriving (Show, Eq, Read)
 
-surface :: Shape -> Float
-surface (Circle _ r) = pi * r ^ 2
-surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+data ParamType a = ParamType { field :: a } deriving(Show)
 
+tellPerson :: Person -> String
+tellPerson (Person { firstName = f, lastName = l, age = a}) = f ++ " " ++ l
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` s = Vector (i*s) (j*s) (k*s)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l+j*m+k*n
+
+fableal = Person { firstName = "Fabrice", lastName = "Leal", age = 23 }
+ret1 = fableal == Person { firstName = "Fabrice", lastName = "Leal", age = 23 }
+ret2 = show fableal
+ret3 = read "Person {firstName = \"Fabrice\", lastName = \"Leal\", age = 23}" :: Person
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+     deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+
+ret4 = Saturday == Saturday
+ret5 = Saturday > Friday
+ret6 = Tuesday < Sunday
+ret7 = Monday `compare` Wednesday
+ret8 = minBound :: Day
+ret9 = maxBound :: Day
+ret10 = succ Thursday
+ret11 = pred Saturday
+ret12 = [Friday .. Sunday]
+ret13 = [minBound .. maxBound] :: [Day]
